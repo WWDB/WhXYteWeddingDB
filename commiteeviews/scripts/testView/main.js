b71d2b9512001ffd24d55877fa0b3da7d4aa651b
Peter Gibbs <pgibbs65@gmail.com>
2018-11-03 23:10:48 -0600

    refactored file structure to match HH server also added hh server files to repo now runs on my local server

--- commiteeviews/scripts/testView/main.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-    
-    var users_table=new TableView("users_table","http://localhost/WhxyteWeddingDB/commiteeviews/TestView.php");
-    
-    users_table.init();
- }
