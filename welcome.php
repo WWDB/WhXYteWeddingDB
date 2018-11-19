c46e5cd84614cbdc4b2e83665412c458fdbe64af
Peter Gibbs <pgibbs65@gmail.com>
2018-11-18 23:29:15 -0700

    fixed Welcome not redirecting to new testview

--- welcome.php
+++ welcome.php
@@ -9,7 +9,7 @@
    
    <body>
       <h1>Welcome <?php echo $_SESSION['login_user']; ?></h1> 
-	  <h2><a href = "/testview/testview.html">Test View</a></h2>
+	  <h2><a href = "/testview/viewdonors.html">Test View</a></h2>
       <h3><a href = "/testview/logout.php">Sign Out</a></h3>
    </body>
    
